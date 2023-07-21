4f275a4b896086a8fa097397967e66fd3ef06bbb
thcainthry <hasani.florinda@gmail.com>
2023-07-21 23:46:28 +0200

    reorganizing

--- flutter_application/lib/food/food_item.dart
+++ flutter_application/lib/food/food_item.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/food.dart';
+import './food.dart';
 
 class FoodItem extends StatelessWidget {
   final Food food;
--- flutter_application/lib/food/main.dart
+++ flutter_application/lib/food/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/food.dart';
-import 'package:flutter_application_1/food_item.dart';
+import './food.dart';
+import './food_item.dart';
 
 void main() {
   runApp(MaterialApp(
